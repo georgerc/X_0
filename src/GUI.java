28752bdac85c63b382fdeb57df3979e0a5a3ac03
georgerc <gcrohozneanu@gmail.com>
2018-07-05 20:51:40 +0300

    First commit

---
+++ src/GUI.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class GUI {
+
+
+    private JPanel panel1;
+
+    public static void main(String[] args){
+            JFrame window=new JFrame("X|0 Game");
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            window.pack();
+            window.setVisible(true);
+            window.setSize(600,600);
+    };
+
+    public void Check_Board(){
+        int rows=3;
+        int cols=3;
+       Button but=new Button();
+
+
+
+
+    }
+};
