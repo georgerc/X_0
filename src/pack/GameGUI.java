dc8a3036e29f98843fd63f8bb4031ee92c200579
georgerc <gcrohozneanu@gmail.com>
2018-07-07 00:27:29 +0300

    Refactor

--- src/pack/GameGUI.java
+++ src/pack/GameGUI.java
@@ -1,42 +1,38 @@
 package pack;
 
-import com.sun.xml.internal.messaging.saaj.soap.JpegDataContentHandler;
+import com.sun.javaws.exceptions.ExitException;
+import com.sun.xml.internal.bind.v2.TODO;
 
-import javax.sound.sampled.Line;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.concurrent.ThreadLocalRandom;
 
-public class App {
-    int nr=0;
+public class GameGUI {
+    int nr = 0;
     private JPanel panel;
-    JFrame window=new JFrame();
-    JButton b[]=new JButton[9];
-    String[] letters=new String[9];
-    JPanel content_holder= new JPanel();
+    JFrame window = new JFrame();
+    JButton b[] = new JButton[9];
+    String[] letters = new String[9];
+    JPanel content_holder = new JPanel();
     JPanel Info_Menu = new JPanel();
-    JPanel Check_Board=new JPanel();
+    JPanel Check_Board = new JPanel();
 
-    public App() {
-
+    public GameGUI(String player_name ) {
         //CONTENT HOLDER//
-        content_holder.setLayout(new BoxLayout(content_holder,BoxLayout.Y_AXIS));
+        content_holder.setLayout(new BoxLayout(content_holder, BoxLayout.Y_AXIS));
         content_holder.setBorder(
-                BorderFactory.createEmptyBorder(5,5,5,5));
+                BorderFactory.createEmptyBorder(5, 5, 5, 5));
         content_holder.setBackground(Color.yellow);
-        Info_Menu.setMaximumSize(new Dimension(500,100));
+        Info_Menu.setMaximumSize(new Dimension(500, 100));
         //INFO MENU BUILD\
-        int tester=1;
-        Info_Menu.setLayout(new FlowLayout(FlowLayout.CENTER,10,10));
-        JLabel Player_Points=new JLabel();
-        Player_Points.setText("Player Points:"+"P");
-        JLabel Computer_Points=new JLabel();
-        Computer_Points.setText("Computer Points:"+"P");
-        JLabel Draws=new JLabel();
-        Draws.setText("Draws:"+"P");
-        JButton New_G_Button=new JButton();
+        int tester = 1;
+        Info_Menu.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        JLabel Player_Points = new JLabel();
+        Player_Points.setText(player_name + ":"+" "+"P");
+        JLabel Computer_Points = new JLabel();
+        Computer_Points.setText("Computer Points:" + "P");
+        JLabel Draws = new JLabel();
+        Draws.setText("Draws:" + "P");
+        JButton New_G_Button = new JButton();
         New_G_Button.setText("New Game");
         Info_Menu.setBackground(Color.pink);
         Info_Menu.add(Player_Points);
@@ -44,24 +40,35 @@ public class App {
         Info_Menu.add(Draws);
         Info_Menu.add(New_G_Button);
         //
-        Check_Board.setLayout(new GridLayout(3,3,2,2));
+        Check_Board.setLayout(new GridLayout(3, 3, 2, 2));
 
         Check_Board.setBackground(Color.white);
-        for(int i=0;i<9;i++) {
+        for (int i = 0; i < 9; i++) {
             b[i] = new JButton();
             b[i].setBackground(Color.black);
             Check_Board.add(b[i]);
         }
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         content_holder.add(Info_Menu);
         content_holder.add(Check_Board);
-        window.setSize(500,500);
+        window.setSize(500, 500);
         window.setContentPane(content_holder);
-
         window.add(Check_Board);
         //window.pack();
         //window.setLocationByPlatform(true);
         window.setVisible(true);
-            /*
+
+            Object[] possibleValues = {"Human", "Computer"};
+            Object selectedValue = JOptionPane.showInputDialog(null,
+                    "Choose one", "Input",
+                    JOptionPane.INFORMATION_MESSAGE, null,
+                    possibleValues, possibleValues[0]);
+        //TODO
+        if(selectedValue==null)
+               window.dispose();
+
+        /*
+
         for(int i=0;i<9;i++)
             letters[i]="";
         window.setSize(500,500);
@@ -99,10 +106,6 @@ public class App {
                     nr+=2;
                 }
             });*/
-        }
-
-    public static void main(String[] args)
-    {
-        new App();
     }
-}
+};
+
