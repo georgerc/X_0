96a88d4030fbbae9adcc6c151ce08ea94db2c5ca
georgerc <gcrohozneanu@gmail.com>
2018-07-09 01:52:48 +0300

    Refactor

--- src/pack/Driver.java
+++ src/pack/Driver.java
@@ -1,27 +1,33 @@
 package pack;
 
 import javax.swing.*;
-import java.util.concurrent.ThreadLocalRandom;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Driver {
+    static private int diff = 0;
+    static private int nr = 0;
+    static private int ln = 0, col = 0;
     private static String ch_C;
     private static String ch_P;
-    private static int diff=0;
-    private  static void random(){
-        diff= ThreadLocalRandom.current().nextInt(0,2);
-    }
-    public void first(){
+    private static JButton b[] = new JButton[9];
+    static private String winner;
+    static private String matrix[][] = new String[5][5];
+    static private String[] letters = new String[9];
+
+
+    static private void first() {
         Object[] possibleValues = {"Human", "Computer"};
         Object selectedValue = JOptionPane.showInputDialog(null,
                 "Who goes first ?", "First Move",
                 JOptionPane.INFORMATION_MESSAGE, null,
                 possibleValues, possibleValues[0]);
         //TODO
-        if(selectedValue=="Human") {
+        //Exeption
+        if (selectedValue == "Human") {
             ch_C = "0";
             ch_P = "X";
-        }
-        else {
+        } else {
             ch_C = "X";
             ch_P = "0";
         }
@@ -29,15 +35,132 @@ public class Driver {
 
     }
 
-    public static void main(String[] args)
-    {
-        Driver  main1=new Driver();
-        String message =JOptionPane.showInputDialog("Please enter name:");
-        GameGUI GUI =new GameGUI(message);
-        main1.first();
-        if(ch_P=="0")
-            GUI.IGoFirst(ch_P,ch_C);
-        GUI.button_response(ch_P,ch_C,diff,message);
+    static private String Validate(int r, int c, String name) {
+        if (matrix[r][c] == matrix[r - 1][c] && matrix[r][c] == matrix[r - 2][c])
+            return name;
+        if (matrix[r][c] == matrix[r - 1][c] && matrix[r][c] == matrix[r + 1][c])
+            return name;
+        if (matrix[r][c] == matrix[r + 1][c] && matrix[r][c] == matrix[r + 2][c])
+            return name;
+        if (c >= 2)
+            if (matrix[r][c] == matrix[r][c - 1] && matrix[r][c] == matrix[r][c - 2])
+                return name;
+        if (matrix[r][c] == matrix[r][c - 1] && matrix[r][c] == matrix[r][c + 1])
+            return name;
+        if (matrix[r][c] == matrix[r][c + 1] && matrix[r][c] == matrix[r][c + 2])
+            return name;
+        if (matrix[r][c] == matrix[r + 1][c + 1] && matrix[r][c] == matrix[r + 2][c + 2])
+            return name;
+        if (r >= 2 && c >= 2)
+            if (matrix[r][c] == matrix[r - 1][c - 1] && matrix[r][c] == matrix[r - 2][c - 2])
+                return name;
+        if (matrix[r][c] == matrix[r - 1][c - 1] && matrix[r][c] == matrix[r + 1][c + 1])
+            return name;
+        if (c >= 2)
+            if (matrix[r][c] == matrix[r + 1][c - 1] && matrix[r][c] == matrix[r + 2][c - 2])
+                return name;
+        if (r >= 2)
+            if (matrix[r][c] == matrix[r - 1][c + 1] && matrix[r][c] == matrix[r - 2][c + 2])
+                return name;
+        if (matrix[r][c] == matrix[r + 1][c - 1] && matrix[r - 1][c + 1] == matrix[r][c])
+            return name;
+        return "NoOne";
+    }
+
+    static private void DisableAll() {
+        for (int i = 0; i < 9; i++)
+            b[i].setEnabled(false);
+    }
+
+    static private void condition(int i) {
+        if (i < 3) {
+            matrix[1][i + 1] = letters[i];
+            ln = 1;
+            col = i + 1;
+        } else if (i < 6) {
+            matrix[2][i - 2] = letters[i];
+            ln = 2;
+            col = i - 2;
+        } else if (i < 9) {
+            matrix[3][i - 5] = letters[i];
+            ln = 3;
+            col = i - 5;
+        }
+
+
+    }
+
+    static private void button_response(String ch_P, String ch_C, int diff, String player_name) {
+        winner = "NoOne";
+        for (int i = 0; i < 9; i++) {
+            b[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    int pos;
+                    for (int i = 0; i < 9; i++) {
+                        if (b[i] == e.getSource()) {
+                            b[i].setText(ch_P);
+                            b[i].setEnabled(false);
+                            letters[i] = ch_P;
+                            //pos = i;
+                            Driver.condition(i);
+                        }
+
+                    }
+                    //Validate
+                    winner = Validate(ln, col, player_name);
+                    if (winner == player_name) {
+                        JOptionPane.showMessageDialog(null, "Winner is " + winner);
+                        DisableAll();
+                        return;
+                    }
+                    int x;
+                    if (diff == 0)
+                        x = (new C_Player_Easy()).choise(letters, ch_P, ch_C, nr);
+                    else
+                        x = (new C_Player_Easy()).choise(letters, ch_P, ch_C, nr);
+                    /*int x = ThreadLocalRandom.current().nextInt(0, 9);
+                    while ((b[x].getText() == ch_P || b[x].getText() == ch_C) && nr < 8) {
+                        x = ThreadLocalRandom.current().nextInt(0, 9);
+                    }*/
+                    if (nr < 8) {
+                        b[x].setText(ch_C);
+                        letters[x] = ch_C;
+                        b[x].setEnabled(false);
+                        nr += 2;
+                        Driver.condition(x);
+                        //Validate
+                        winner = Validate(ln, col, "Computer");
+                        if (winner == "Computer") {
+                            JOptionPane.showMessageDialog(null, "Winner is Computer!");
+                            DisableAll();
+                            return;
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    static private void IGoFirst(String ch_P, String ch_C) {
+        int x;
+        x = (new C_Player_Easy()).choise(null, ch_P, ch_C, 0);
+        b[x].setText(ch_C);
+        b[x].setEnabled(false);
+        letters[x] = ch_C;
+    }
+
+    public static void main(String[] args) {
+        // Driver  main1=new Driver();
+        String message = JOptionPane.showInputDialog("Please enter name:");
+        GameGUI GUI = new GameGUI(message);
+        Driver.first();
+        b = GUI.get_buttons();
+        if (ch_P == "0")
+            Driver.IGoFirst(ch_P, ch_C);
+        Driver.button_response(ch_P, ch_C, diff, message);
+
+
     }
 }
 
--- src/pack/GameGUI.java
+++ src/pack/GameGUI.java
@@ -10,148 +10,13 @@ import java.awt.event.ActionListener;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class GameGUI {
-    int nr=0;
-    int ln=0,col=0;
     private JFrame window = new JFrame();
     private JButton b[] = new JButton[9];
-    private String[] letters = new String[9];
     private JPanel content_holder = new JPanel();
     private JPanel Info_Menu = new JPanel();
     private JPanel Check_Board = new JPanel();
-    private String matrix[][]=new String[5][5];
-    private String winner=new String();
-    public  String get_Button_text(int x){
-       return  b[x].getText();
-    }
-
-    public void set_Button(int x, String s) {
-        b[x].setText(s);
-    }
-    public String Validate(int r,int c,String name)
-    {
-        if(matrix[r][c]==matrix[r-1][c] && matrix[r][c]==matrix[r-2][c])
-            return name;
-        if(matrix[r][c]==matrix[r-1][c] && matrix[r][c]==matrix[r+1][c])
-            return name;
-        if(matrix[r][c]==matrix[r+1][c] && matrix[r][c]==matrix[r+2][c])
-            return name;
-        if(c>=2)
-            if(matrix[r][c]==matrix[r][c-1] && matrix[r][c]==matrix[r][c-2])
-                return name;
-        if(matrix[r][c]==matrix[r][c-1] && matrix[r][c]==matrix[r][c+1])
-            return name;
-        if(matrix[r][c]==matrix[r][c+1] && matrix[r][c]==matrix[r][c+2])
-            return name;
-        if(matrix[r][c]==matrix[r+1][c+1] && matrix[r][c]==matrix[r+2][c+2])
-            return name;
-        if(r>=2 && c>=2)
-            if(matrix[r][c]==matrix[r-1][c-1] && matrix[r][c]==matrix[r-2][c-2])
-                 return name;
-        if(matrix[r][c]==matrix[r-1][c-1] && matrix[r][c]==matrix[r+1][c+1])
-            return name;
-        if(c>=2)
-            if(matrix[r][c]==matrix[r+1][c-1] && matrix[r][c]==matrix[r+2][c-2])
-                 return name;
-        if(r>=2)
-            if(matrix[r][c]==matrix[r-1][c+1] && matrix[r][c]==matrix[r-2][c+2])
-                 return name;
-        if(matrix[r][c]==matrix[r+1][c-1] && matrix[r-1][c+1]==matrix[r][c])
-            return name;
-        return "NoOne";
-    }
-    private void DisableAll()
-    {
-        for(int i=0;i<9;i++)
-            b[i].setEnabled(false);
-    }
-    public void button_response(String ch_P,String ch_C,int diff,String player_name){
-        winner="NoOne";
-        for(int i=0;i<9;i++) {
-            b[i].addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    int pos;
-                    for (int i = 0; i < 9; i++) {
-                        if (b[i] == e.getSource()) {
-                            set_Button(i, ch_P);
-                            b[i].setEnabled(false);
-                            letters[i]=ch_P;
-                            //pos = i;
-                            if(i<3) {
-                                matrix[1][i+1] = letters[i];
-                                ln = 1;
-                                col = i+1;
-                            }
-                            else
-                                if(i<6) {
-                                    matrix[2][i - 2] = letters[i];
-                                    ln = 2;
-                                    col = i-2;
-                                }
-                                else
-                                    if(i<9) {
-                                        matrix[3][i - 5] = letters[i];
-                                        ln = 3;
-                                        col = i - 5;
-                                    }
-                        }
-                    }
-                    //Validate
-                    winner=Validate(ln,col,player_name);
-                    if(winner==player_name)
-                    {
-                        JOptionPane.showMessageDialog(null,"Winner is "+winner);
-                        DisableAll();
-                        return;
-                    }
-                    int x;
-                    if(diff==0)
-                        x=(new C_Player_Easy()).choise(letters,ch_P,ch_C,nr);
-                    else
-                        x=(new C_Player_Easy()).choise(letters,ch_P,ch_C,nr);
-                    /*int x = ThreadLocalRandom.current().nextInt(0, 9);
-                    while ((b[x].getText() == ch_P || b[x].getText() == ch_C) && nr < 8) {
-                        x = ThreadLocalRandom.current().nextInt(0, 9);
-                    }*/
-                    if (nr < 8) {
-                        b[x].setText(ch_C);
-                        letters[x] = ch_C;
-                        b[x].setEnabled(false);
-                        nr += 2;
-                        if (x < 3) {
-                            matrix[1][x + 1] = letters[x];
-                            ln = 1;
-                            col = x + 1;
-                        } else if (x < 6) {
-                            matrix[2][x - 2] = letters[x];
-                            ln = 2;
-                            col = x - 2;
-                        } else if (x < 9) {
-                            matrix[3][x - 5] = letters[x];
-                            ln = 3;
-                            col = x - 5;
-                        }
-                        //Validate
-                        winner = Validate(ln, col, "Computer");
-                        if (winner == "Computer") {
-                            JOptionPane.showMessageDialog(null, "Winner is Computer!");
-                            DisableAll();
-                            return;
-                        }
-                    /*if ( (b[0].getText() == b[4].getText() && b[4].getText() == b[8].getText() ) && b[0].getText()!=" ")
-                    JOptionPane.showMessageDialog(null, b[0].getText() + "WON");*/
-                    }
-                }
-            });
-        }
-    }
-    public void IGoFirst(String ch_P,String ch_C)
-    {
-        int x;
-        x=(new C_Player_Easy()).choise(null,ch_P,ch_C,0);
-        set_Button(x,ch_C);
-        b[x].setEnabled(false);
-        letters[x]=ch_C;
+    public  JButton[] get_buttons(){
+        return b;
     }
     public GameGUI(String player_name) {
         //CONTENT HOLDER BUILDER//
@@ -176,9 +41,7 @@ public class GameGUI {
         Info_Menu.add(Computer_Points);
         Info_Menu.add(Draws);
         Info_Menu.add(New_G_Button);
-        //
         Check_Board.setLayout(new GridLayout(3, 3, 2, 2));
-
         Check_Board.setBackground(Color.white);
         for (int i = 0; i < 9; i++) {
             b[i] = new JButton();
@@ -191,95 +54,8 @@ public class GameGUI {
         window.setSize(500, 500);
         window.setContentPane(content_holder);
         window.add(Check_Board);
-        //window.pack();
-        //window.setLocationByPlatform(true);
         window.setVisible(true);
-
-        /*Object[] possibleValues = {"Human", "Computer"};
-        Object selectedValue = JOptionPane.showInputDialog(null,
-                "Who goes first ?", "First Move",
-                JOptionPane.INFORMATION_MESSAGE, null,
-                possibleValues, possibleValues[0]);
-        //TODO
-        final String ch_C;
-        final String ch_P;
-
-
-        if(selectedValue=="Human") {
-            ch_C = "0";
-            ch_P = "X";
         }
-        else {
-            ch_C = "X";
-            ch_P = "0";
-        }*/
-
-        // int game_dif=ThreadLocalRandom.current().nextInt(0,1);
-        // Ai=new C_Player_Easy();
-        // Ai.choise();
-        /*if(game_dif==1)
-            Ai=new C_Player_Hard();
-        else {
-            Ai = new C_Player_Easy();
-            Ai.choise();
-        }*/
-      /*  for(int i=0;i<9;i++)
-            letters[i]="";
-        window.setSize(500,500);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setLayout(new GridLayout(3,3));
-        for(int i=0;i<9;i++)
-        {
-            b[i]=new JButton();
-            b[i].setBackground(Color.yellow);
-            window.add(b[i]);
-        }
-        window.setVisible(true);
-*/
-
-    //  System.out.printf(b[0].getText());
-
-       /* for(int i=0;i<9;i++) {
-
-
-                       b[i].addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    int pos;
-                    for (int i = 0; i < 9; i++) {
-                        if (b[i] == e.getSource()) {
-                            set_Button(i,ch_P);
-                            pos = i;
-                        }
-                    }
-                    int x = ThreadLocalRandom.current().nextInt(0, 9);
-                    while ((b[x].getText() == ch_P || b[x].getText() == ch_C) && nr < 8) {
-                        x = ThreadLocalRandom.current().nextInt(0, 9);
-                    }
-                    if (nr < 8)
-                        b[x].setText(ch_C);
-                    nr += 2;
-
-                    *//*if ( (b[0].getText() == b[4].getText() && b[4].getText() == b[8].getText() ) && b[0].getText()!=" ")
-                        JOptionPane.showMessageDialog(null, b[0].getText() + "WON");*//*
-                }
-            });*/
-
-
-
-        }
-
-
-
-
-            int upper=1;
-            int counter=0;
-            int oc=0;
-
-            // check diagonal
-
-
-
             }
 
 
