beb4fc739812b46aad9377dffdc6d02efeba01ce
georgerc <gcrohozneanu@gmail.com>
2018-07-08 14:17:44 +0300

    <>

--- src/pack/GameGUI.java
+++ src/pack/GameGUI.java
@@ -5,29 +5,37 @@ import com.sun.xml.internal.bind.v2.TODO;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class GameGUI {
     int nr = 0;
     private JPanel panel;
-    JFrame window = new JFrame();
-    JButton b[] = new JButton[9];
-    String[] letters = new String[9];
-    JPanel content_holder = new JPanel();
-    JPanel Info_Menu = new JPanel();
-    JPanel Check_Board = new JPanel();
+    private JFrame window = new JFrame();
+    public JButton b[] = new JButton[9];
+    private String[] letters = new String[9];
+    private JPanel content_holder = new JPanel();
+    private JPanel Info_Menu = new JPanel();
+    private JPanel Check_Board = new JPanel();
+    private Computer_Player Ai;
 
-    public GameGUI(String player_name ) {
-        //CONTENT HOLDER//
+    public void set_Button(int x, String s) {
+        b[x].setText(s);
+    }
+
+    public GameGUI(String player_name) {
+        //CONTENT HOLDER BUILDER//
         content_holder.setLayout(new BoxLayout(content_holder, BoxLayout.Y_AXIS));
         content_holder.setBorder(
                 BorderFactory.createEmptyBorder(5, 5, 5, 5));
         content_holder.setBackground(Color.yellow);
         Info_Menu.setMaximumSize(new Dimension(500, 100));
-        //INFO MENU BUILD\
+        //INFO MENU BUILDER\
         int tester = 1;
         Info_Menu.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 10));
         JLabel Player_Points = new JLabel();
-        Player_Points.setText(player_name + ":"+" "+"P");
+        Player_Points.setText(player_name + ":" + " " + "P");
         JLabel Computer_Points = new JLabel();
         Computer_Points.setText("Computer Points:" + "P");
         JLabel Draws = new JLabel();
@@ -44,8 +52,8 @@ public class GameGUI {
 
         Check_Board.setBackground(Color.white);
         for (int i = 0; i < 9; i++) {
-            b[i] = new JButton();
-            b[i].setBackground(Color.black);
+            b[i] = new JButton(" ");
+            b[i].setBackground(Color.yellow);
             Check_Board.add(b[i]);
         }
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -58,18 +66,35 @@ public class GameGUI {
         //window.setLocationByPlatform(true);
         window.setVisible(true);
 
-            Object[] possibleValues = {"Human", "Computer"};
-            Object selectedValue = JOptionPane.showInputDialog(null,
-                    "Choose one", "Input",
-                    JOptionPane.INFORMATION_MESSAGE, null,
-                    possibleValues, possibleValues[0]);
+        Object[] possibleValues = {"Human", "Computer"};
+        Object selectedValue = JOptionPane.showInputDialog(null,
+                "Who goes first ?", "First Move",
+                JOptionPane.INFORMATION_MESSAGE, null,
+                possibleValues, possibleValues[0]);
         //TODO
-        if(selectedValue==null)
-               window.dispose();
+        final String ch_C;
+        final String ch_P;
 
-        /*
 
-        for(int i=0;i<9;i++)
+        if(selectedValue=="Human") {
+            ch_C = "0";
+            ch_P = "X";
+        }
+        else {
+            ch_C = "X";
+            ch_P = "0";
+        }
+
+        // int game_dif=ThreadLocalRandom.current().nextInt(0,1);
+        // Ai=new C_Player_Easy();
+        // Ai.choise();
+        /*if(game_dif==1)
+            Ai=new C_Player_Hard();
+        else {
+            Ai = new C_Player_Easy();
+            Ai.choise();
+        }*/
+      /*  for(int i=0;i<9;i++)
             letters[i]="";
         window.setSize(500,500);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -80,32 +105,54 @@ public class GameGUI {
             b[i].setBackground(Color.yellow);
             window.add(b[i]);
         }
-
         window.setVisible(true);
 */
-        /*for(int i=0;i<9;i++)
-        {
+
+      System.out.printf(b[0].getText());
+
+        for(int i=0;i<9;i++) {
+
+
             b[i].addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     int pos;
-                    for(int i=0;i<9;i++)
-                    {
-                        if(b[i]==e.getSource())
-                        {
-                            b[i].setText("X");
-                            pos=i;
+                    for (int i = 0; i < 9; i++) {
+                        if (b[i] == e.getSource()) {
+                           set_Button(i,ch_P);
+                            pos = i;
                         }
                     }
                     int x = ThreadLocalRandom.current().nextInt(0, 9);
-                    while((b[x].getText()=="X" || b[x].getText()=="0") && nr<8) {
+                    while ((b[x].getText() == ch_P || b[x].getText() == ch_C) && nr < 8) {
                         x = ThreadLocalRandom.current().nextInt(0, 9);
                     }
-                    if(nr<8)
-                        b[x].setText("0");
-                    nr+=2;
+                    if (nr < 8)
+                        b[x].setText(ch_C);
+                    nr += 2;
+                    if ( (b[0].getText() == b[4].getText() && b[4].getText() == b[8].getText() ) && b[0].getText()!=" ")
+                        JOptionPane.showMessageDialog(null, b[0].getText() + "WON");
                 }
-            });*/
+
+
+            });
+
+
+
+        }
+
+
+
+            int upper=1;
+            int counter=0;
+            int oc=0;
+
+            // check diagonal
+
+
+
+            }
     }
-};
+
+
 
