7fc6658ae7dcef26c732a9edf4615156ec14efdb
georgerc <gcrohozneanu@gmail.com>
2018-07-14 23:29:26 +0300

    JavaDoc x2

--- src/pack/C_Player_Easy.java
+++ src/pack/C_Player_Easy.java
@@ -6,8 +6,15 @@ import java.util.concurrent.ThreadLocalRandom;
 public class C_Player_Easy extends  Computer_Player {
 
 
-
-public int choise(String b[] ,String ch_P, String ch_C,int nr){
+    /**
+     * Pick a random number that represents the choise of the computer.
+     * @param b The buttons
+     * @param ch_P The character of the computer player(either "0" or "X").
+     * @param ch_C The character of the human player(either "0"or "X")
+     * @param nr the number of occupied buttons.
+     * @return Returns a random number between 0 and 9,that represents the move of the computer player.
+     */
+    public int choise(String b[] ,String ch_P, String ch_C,int nr){
 
     int x = ThreadLocalRandom.current().nextInt(0, 9);
     if(b!=null) {
