1b5383025cc3ec2bf93a9b75e2cab4533db12f66
georgerc <gcrohozneanu@gmail.com>
2018-07-14 23:26:11 +0300

    JavaDoc

--- src/pack/Computer_Player.java
+++ src/pack/Computer_Player.java
@@ -1,16 +1,16 @@
 package pack;
 import java.util.concurrent.ThreadLocalRandom;
-public class Computer_Player {
 
+/**
+ * Base class for the Computer Players.
+ */
+public abstract class Computer_Player {
 
-public int choise(String b[] ,String ch_P, String ch_C,int nr){
-
-    int x = ThreadLocalRandom.current().nextInt(0, 9);
-    if(b!=null) {
-        while ((b[x] == ch_P || b[x] == ch_C) && nr < 9) {
-            x = ThreadLocalRandom.current().nextInt(0, 9);
-        }
+    /**
+     * Prototype function for
+     */
+    public int choise(String b[] ,String ch_P, String ch_C,int nr){
+        return 1;
     }
-    return x;
 }
-}
+
